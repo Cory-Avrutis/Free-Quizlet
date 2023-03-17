f9c6d07042f12b57cffd38f081624a12269eafe0
William Steven Matiz <william.s.matiz@gmail.com>
2023-03-17 17:32:21 -0400

    Update models.py, add user.get_id() Now user can be part of the dictionary in the views.py file, such that it's also a key and value in addition to the terms/defs and title

--- website/models.py
+++ website/models.py
@@ -1,21 +1,8 @@
 from flask_login import UserMixin
 
-class User:
-    def __init__(self,username):
+class User(UserMixin):
+    def __init__(self, username):
         self.username = username
-    
-    @staticmethod
-    def is_authenticated():
-        return True
-    
-    @staticmethod
-    def is_active():
-        return True
-    
-    @staticmethod
-    def is_anonymous():
-        return True
 
     def get_id(self):
         return self.username
-
--- website/views.py
+++ website/views.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, url_for, redirect, render_template, request
 from flask_login import login_required, current_user
 
+from .models import User
+
 
 # Define a blueprint called "views" for webpage viewing
 views = Blueprint("views", __name__)
@@ -17,12 +19,23 @@ def home():
 @login_required
 def create_set():
     if request.method == 'POST':
+
+        user = User(current_user)
+
+        print("The user's name is ", user.get_id())
         numcards = int(request.form.get('num_cards'))    
         card_set = {}
+
+        title = request.form.get(f'title')
         for i in range(1,numcards+1):
             term = request.form.get(f'term_{i}')
             defn = request.form.get(f'definition_{i}')
-            card_set[term] = defn
+
+            if term != "" and defn != "" and title != "":
+                card_set[term] = defn
+                card_set["Title"] = title 
+                card_set["User"] = current_user.get_id()   #<-- cash money
         print(card_set)
+        
     return render_template("create_set.html", user=current_user)
 
