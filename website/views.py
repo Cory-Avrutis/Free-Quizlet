8afe47e037da1e7d9313cbc01371538893a5a0ec
William Steven Matiz <william.s.matiz@gmail.com>
2023-04-03 23:27:49 -0400

    Add edit definition feature

--- website/views.py
+++ website/views.py
@@ -111,8 +111,6 @@ def edit_title():
         old_title = request.form['old_title']
         new_title = request.form['new_title']
 
-    print("My test\nThe old title retrieved is ", old_title)
-    print("The new title retrieved is ", new_title)
 
     card_sets.update_one( 
         {"User": current_user.get_id(), "Title": old_title}, #query to extract which title
@@ -144,16 +142,67 @@ def edit_term():
     cardSet = card_sets.find_one( {"User": current_user.get_id(), "Title": title} ) # find set that needs editing
     cards = cardSet['Cards']
     
-    # add new card with new ter, use existing definiton
 
-    card_sets.update_one({"User": current_user.get_id(), "Title": title}, {"$set": {'Cards.' + new_term: cards[old_term]}}) #adds new term, matches it with old definition
-
+   
     #To Mariela, 
-    #what happens in the line above if the new term is a term that already exists? 
+    #what happens in if the new term is a term that already exists? 
     #do we need to add error handling, is it an automatic reject by Mongo, or does it not even matter? 
     # - Steven
 
-    card_sets.update_one({"User": current_user.get_id(), "Title": title}, {"$unset": {'Cards.' + old_term: cards[old_term]}}) #gets rid of the previous term and its definition
+    """
+    some discoveries on above
+    1) updated term being the same as its old term leads to the card being deleted (handled with if old_term != new_term)
+
+    2) updated term being the same as another term already in the set leads to undefined behavior. 
+     usually deletes both old and other shared card with same term, sometimes does nothing, and rarely will fatally crash website
+       (for Mariela to do. i assume something like if new term is not in cards_terms. put it with the if condition below)
+
+
+
+    3) not a dangeorus bug. when a card is updated, it is appended at the end of the data structure. this reorders it differently when edit_set renders
+        could be fixed. could also be left as is
+
+        - Steven
+    """
+
+    if old_term != new_term:
+
+        # add new card with new ter, use existing definiton
+        card_sets.update_one({"User": current_user.get_id(), "Title": title}, {"$set": {'Cards.' + new_term: cards[old_term]}})     #adds new term, matches it with old definition
+        card_sets.update_one({"User": current_user.get_id(), "Title": title}, {"$unset": {'Cards.' + old_term: cards[old_term]}})   #gets rid of the previous term and its definition
+    
+    cardSet = card_sets.find_one( {"User": current_user.get_id(), "Title": title} ) # finds the updated cards to be sent to edit_set.html 
+    cards = cardSet['Cards']
+    
+    return render_template("edit_set.html", user=current_user, cards=cards, title=title)
+
+
+@views.route("/edit_definition", methods = ['POST', 'GET'])
+@login_required
+def edit_definition():
+
+    title = ""
+    term = ""
+    old_def = ""
+    new_def =""
+    
+    if request.method == 'POST':
+        title = request.form['title']
+        term = request.form['term']
+        old_def = request.form['old_def']
+        new_def = request.form['new_def']
+
+    """
+    To Mariela,
+
+    please also handle error checking to make sure new definition is not already a definition in the set
+
+     thank you! - Steven
+    """
+    
+    if old_def != new_def:
+        card_sets.update_one({"User": current_user.get_id(), "Title": title}, {"$unset": {'Cards.' + term: old_def}})     #gets rid of the previous term and its definition
+        card_sets.update_one({"User": current_user.get_id(), "Title": title}, {"$set": {'Cards.' + term: new_def}})       #adds new term, matches it with old definition     
 
     cardSet = card_sets.find_one( {"User": current_user.get_id(), "Title": title} ) # resets the updates cards to be sent to edit_set.html 
     cards = cardSet['Cards']
